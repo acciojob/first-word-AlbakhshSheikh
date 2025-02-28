629806ddce872a5953cb70b1402ef1e72405f3df
Albakhsh Sheikh <98633969+AlbakhshSheikh@users.noreply.github.com>
2025-02-28 21:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstWord(s) {
   // your code here
+	s = s.trim(); 
+    let index = s.indexOf(" ");
+    return index === -1 ? s : s.substring(0, index);
 }
 
 // Do not change the code below
